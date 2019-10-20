700bb9238caf26c1640b488d2f85ae5d2049a750
grymyk <grymyk@gmail.com>
2019-10-20 09:11:51 +0300

    rotate

--- public/globe.js
+++ public/globe.js
@@ -33,9 +33,9 @@ DAT.Globe = function(container, colorFn) {
         'varying vec3 vNormal;',
         'varying vec2 vUv;',
         'void main() {',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-          'vNormal = normalize( normalMatrix * normal );',
-          'vUv = uv;',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        'vNormal = normalize( normalMatrix * normal );',
+        'vUv = uv;',
         '}'
       ].join('\n'),
       fragmentShader: [
@@ -43,10 +43,10 @@ DAT.Globe = function(container, colorFn) {
         'varying vec3 vNormal;',
         'varying vec2 vUv;',
         'void main() {',
-          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-          'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+        'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+        'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+        'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+        'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
       ].join('\n')
     },
@@ -55,15 +55,15 @@ DAT.Globe = function(container, colorFn) {
       vertexShader: [
         'varying vec3 vNormal;',
         'void main() {',
-          'vNormal = normalize( normalMatrix * normal );',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        'vNormal = normalize( normalMatrix * normal );',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
         '}'
       ].join('\n'),
       fragmentShader: [
         'varying vec3 vNormal;',
         'void main() {',
-          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+        'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+        'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
         '}'
       ].join('\n')
     }
@@ -111,11 +111,11 @@ DAT.Globe = function(container, colorFn) {
 
     material = new THREE.ShaderMaterial({
 
-          uniforms: uniforms,
-          vertexShader: shader.vertexShader,
-          fragmentShader: shader.fragmentShader
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader
 
-        });
+    });
 
     mesh = new THREE.Mesh(geometry, material);
     mesh.rotation.y = Math.PI;
@@ -126,14 +126,14 @@ DAT.Globe = function(container, colorFn) {
 
     material = new THREE.ShaderMaterial({
 
-          uniforms: uniforms,
-          vertexShader: shader.vertexShader,
-          fragmentShader: shader.fragmentShader,
-          side: THREE.BackSide,
-          blending: THREE.AdditiveBlending,
-          transparent: true
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true
 
-        });
+    });
 
     mesh = new THREE.Mesh(geometry, material);
     mesh.scale.set( 1.1, 1.1, 1.1 );
@@ -217,10 +217,6 @@ DAT.Globe = function(container, colorFn) {
 
       scene.add(this.points);
     }
-  }
-
-  function addNoMad() {
-      console.log('addNoMad');
   }
 
   function addPoint(lat, lng, size, color, subgeo) {
@@ -323,8 +319,8 @@ DAT.Globe = function(container, colorFn) {
   }
 
   function animate() {
-    requestAnimationFrame(animate);
 
+    requestAnimationFrame(animate);
     render();
   }
 
@@ -333,8 +329,9 @@ DAT.Globe = function(container, colorFn) {
 
     // rotation.x += (target.x - rotation.x) * 0.1;
     // rotation.y += (target.y - rotation.y) * 0.1;
-
-    camera.rotation.z += 0.01;
+    //
+    rotation.x += 0.001;
+    rotation.y += 0.001;
 
     distance += (distanceTarget - distance) * 0.3;
 
@@ -342,12 +339,15 @@ DAT.Globe = function(container, colorFn) {
     camera.position.y = distance * Math.sin(rotation.y);
     camera.position.z = distance * Math.cos(rotation.x) * Math.cos(rotation.y);
 
-      renderer.render(scene, camera);
+    camera.lookAt(mesh.position);
+
+    renderer.render(scene, camera);
   }
 
   init();
-
   this.animate = animate;
+  animate()
+
 
   this.__defineGetter__('time', function() {
     return this._time || 0;
@@ -379,7 +379,6 @@ DAT.Globe = function(container, colorFn) {
 
   this.addData = addData;
   this.createPoints = createPoints;
-  this.addNoMad = addNoMad;
   this.renderer = renderer;
   this.scene = scene;
 
